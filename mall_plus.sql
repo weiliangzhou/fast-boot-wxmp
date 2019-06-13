50038863149cf12dc4532452d967aa2f02677811
weiliangzhou <382308664@qq.com>
2019-06-13 18:24:36 +0800

    去除shiro jwt

--- mall_plus.sql
+++ mall_plus.sql
@@ -1,7 +1,41 @@
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : 本机
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost:3306
+ Source Schema         : mall_plus
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+
+ Date: 13/06/2019 18:24:01
+*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_operation_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_operation_log`;
+CREATE TABLE `sys_operation_log`  (
+  `operation_log_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `log_description` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '日志描述',
+  `action_args` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '方法参数',
+  `user_no` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户主键',
+  `class_name` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类名称',
+  `method_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '方法名称',
+  `ip` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
+  `model_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '模块名称',
+  `action` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作',
+  `succeed` int(2) NULL DEFAULT NULL COMMENT '是否成功 1:成功 2异常',
+  `message` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '异常堆栈信息',
+  PRIMARY KEY (`operation_log_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '操作日志表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_permission
@@ -102,22 +136,32 @@ CREATE TABLE `sys_user_role`  (
 INSERT INTO `sys_user_role` VALUES (1, 1, 1);
 INSERT INTO `sys_user_role` VALUES (2, 2, 2);
 
+-- ----------------------------
+-- Table structure for user_base
+-- ----------------------------
+DROP TABLE IF EXISTS `user_base`;
+CREATE TABLE `user_base`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cellphone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机',
+  `head_img_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `gzh_open_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '公众号openid',
+  `xcx_open_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '小程序openid',
+  `union_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'unionid',
+  `register_from` int(2) NULL DEFAULT NULL COMMENT '注册来源 1:h5 2:android 3:ios 4:小程序',
+  `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `version` int(11) NULL DEFAULT NULL,
+  `deleted` tinyint(1) NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-DROP TABLE IF EXISTS `sys_operation_log`;
-CREATE TABLE `sys_operation_log` (
-  `operation_log_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `log_description` varchar(64) DEFAULT NULL COMMENT '日志描述',
-  `action_args` varchar(300) DEFAULT NULL COMMENT '方法参数',
-  `user_no` varchar(50) DEFAULT NULL COMMENT '用户主键',
-  `class_name` varchar(300) DEFAULT NULL COMMENT '类名称',
-  `method_name` varchar(64) DEFAULT NULL COMMENT '方法名称',
-  `ip` varchar(32) DEFAULT NULL,
-  `create_time` bigint(20) DEFAULT NULL COMMENT '创建时间',
-  `model_name` varchar(50) DEFAULT NULL COMMENT '模块名称',
-  `action` varchar(50) DEFAULT NULL COMMENT '操作',
-  `succeed` int(2) DEFAULT NULL COMMENT '是否成功 1:成功 2异常',
-  `message` longtext COMMENT '异常堆栈信息',
-  PRIMARY KEY (`operation_log_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=203 DEFAULT CHARSET=utf8 COMMENT='操作日志表';
+-- ----------------------------
+-- Records of user_base
+-- ----------------------------
+INSERT INTO `user_base` VALUES (4, NULL, 'http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83ep1anSA3Jxibms1BrdTpxSkfL0UMATDwSkgzPicgh0hialao5v6GhummpC7cnxyNBupkHb3UktajnLdQ/132', 'A二师兄🏆', 'o0h5h1Vvlmp7Z4leij7gnuTg65ak', NULL, NULL, 1, '2019-06-13 18:06:32', '2019-06-13 18:06:32', NULL, 0);
+INSERT INTO `user_base` VALUES (5, NULL, 'http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83ep1anSA3Jxibms1BrdTpxSkfL0UMATDwSkgzPicgh0hialao5v6GhummpC7cnxyNBupkHb3UktajnLdQ/132', 'A二师兄🏆', 'o0h5h1Vvlmp7Z4leij7gnuTg65ak', NULL, NULL, 1, '2019-06-13 18:07:25', '2019-06-13 18:07:25', NULL, 0);
+INSERT INTO `user_base` VALUES (6, NULL, 'http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83ep1anSA3Jxibms1BrdTpxSkfL0UMATDwSkgzPicgh0hialao5v6GhummpC7cnxyNBupkHb3UktajnLdQ/132', 'A二师兄🏆', 'o0h5h1Vvlmp7Z4leij7gnuTg65ak', NULL, NULL, 1, '2019-06-13 18:08:08', '2019-06-13 18:08:08', NULL, 0);
+INSERT INTO `user_base` VALUES (7, NULL, 'http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83ep1anSA3Jxibms1BrdTpxSkfL0UMATDwSkgzPicgh0hialao5v6GhummpC7cnxyNBupkHb3UktajnLdQ/132', 'A二师兄🏆', 'o0h5h1Vvlmp7Z4leij7gnuTg65ak', NULL, NULL, 1, '2019-06-13 18:17:02', '2019-06-13 18:17:02', NULL, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
