391a55a1dc30d4a317c3868ad597d05440c25229
382308664@qq.com <382308664@qq.com>
2019-06-26 17:03:27 +0800

    加入查询余额接口

--- web/src/main/java/com/zwl/mall/controller/RewardController.java
+++ web/src/main/java/com/zwl/mall/controller/RewardController.java
@@ -2,7 +2,13 @@ package com.zwl.mall.controller;
 
 import com.zwl.common.base.Result;
 import com.zwl.common.base.ResultUtil;
+import com.zwl.mall.dao.model.UserBase;
+import com.zwl.mall.system.annotation.CurrentUser;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,8 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
+@RequestMapping("/api/reward")
+@Api(tags = "当前余额")
 public class RewardController {
-    public Result getCurrentReward() {
+    @GetMapping("/info")
+    @ApiOperation(value = "获取当前BTC", notes = "获取当前BTC")
+    public Result getCurrentReward(@CurrentUser UserBase userBase) {
+        // TODO: 2019/6/26 根据算力计算当前btc
+        log.info(userBase.getNickName());
 
         return ResultUtil.ok("ok");
     }
